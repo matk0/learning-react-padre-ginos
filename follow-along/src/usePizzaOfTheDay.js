35e35085a58485a91239d49943cf3de32b78f739
matk0 <matej@occupationbliss.com>
2025-02-06 09:29:21 +0100

    Finalize Cart checkout.

---
+++ follow-along/src/usePizzaOfTheDay.js
@@ -0,0 +1,22 @@
+import { useState, useEffect, useDebugValue } from "react";
+
+export const usePizzaOfTheDay = () => {
+  const [pizzaOfTheDay, setPizzaOfTheDay] = useState(null);
+  useDebugValue(
+    pizzaOfTheDay
+      ? `${pizzaOfTheDay.id} : ${pizzaOfTheDay.name}`
+      : "loading...",
+  );
+
+  useEffect(() => {
+    async function fetchPizzaOfTheDay() {
+      const response = await fetch("/api/pizza-of-the-day");
+      const json = await response.json();
+      setPizzaOfTheDay(json);
+    }
+
+    fetchPizzaOfTheDay();
+  }, []);
+
+  return pizzaOfTheDay;
+};
