03a92b7f798bd1d1a236c37ce1f8692901ec183a
matk0 <matej@occupationbliss.com>
2025-02-06 10:28:54 +0100

    Add Header and Cart using context.

--- follow-along/src/App.jsx
+++ follow-along/src/App.jsx
@@ -1,20 +1,25 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import Order from "./Order";
-import PizzaOfTheDay from "./PizzaOfTheDay";
+import { StrictMode, useState } from 'react';
+import { createRoot } from 'react-dom/client';
+import Order from './Order';
+import PizzaOfTheDay from './PizzaOfTheDay';
+import Header from './Header';
+import { CartContext } from './contexts';
 
 const App = () => {
+  const cartHook = useState([]);
   return (
     <StrictMode>
-      <div>
-        <h1 className="logo">Padre Gino's - Order Now</h1>
-        <Order />
-        <PizzaOfTheDay />
-      </div>
+      <CartContext.Provider value={cartHook}>
+        <div>
+          <Header />
+          <Order />
+          <PizzaOfTheDay />
+        </div>
+      </CartContext.Provider>
     </StrictMode>
   );
 };
 
-const container = document.getElementById("root");
+const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(<App />);
