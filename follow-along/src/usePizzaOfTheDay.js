ba6a243042d12555ed4c81070b49f7fcb980ecde
Matej <matej@occupationbliss.com>
2025-02-23 07:20:50 +0100

    Add custom hooks test.

--- follow-along/src/usePizzaOfTheDay.js
+++ follow-along/src/usePizzaOfTheDay.js
@@ -1,16 +1,16 @@
-import { useState, useEffect, useDebugValue } from "react";
+import { useState, useEffect, useDebugValue } from 'react';
 
 export const usePizzaOfTheDay = () => {
   const [pizzaOfTheDay, setPizzaOfTheDay] = useState(null);
   useDebugValue(
     pizzaOfTheDay
       ? `${pizzaOfTheDay.id} : ${pizzaOfTheDay.name}`
-      : "loading...",
+      : 'loading...',
   );
 
   useEffect(() => {
     async function fetchPizzaOfTheDay() {
-      const response = await fetch("/api/pizza-of-the-day");
+      const response = await fetch('/api/pizza-of-the-day');
       const json = await response.json();
       setPizzaOfTheDay(json);
     }
